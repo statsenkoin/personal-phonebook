f14f014f66642d185be1356b4fce7ea41629900a
statsenkoin <34393845+statsenkoin@users.noreply.github.com>
2023-04-14 12:30:31 +0000

    Deploying to gh-pages from @ statsenkoin/personal-phonebook@f932ca030b32a2b9552d2341f761ba2e569df15a 🚀

---
+++ static/js/427.814473de.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[427],{7427:function(e,t,n){n.r(t),n.d(t,{default:function(){return a}});var s=n(7729),r=n(4554),i=n(890),c=n(7924),l=n(3329);function a(){return(0,l.jsxs)(r.Z,{sx:{display:{xs:"flex"},alignItems:"center",flexDirection:"column"},children:[(0,l.jsx)(c.Z,{sx:{width:1,pt:2,pb:1},children:(0,l.jsx)(i.Z,{variant:"h5",sx:{fontWeight:700},children:"Login"})}),(0,l.jsx)(s.U0,{}),(0,l.jsx)(c.Z,{sx:{width:1,pt:2,pb:1}})]})}}}]);
+//# sourceMappingURL=427.814473de.chunk.js.map
