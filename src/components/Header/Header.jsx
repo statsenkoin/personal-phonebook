99dbd4f773ad448ce6e6d58e07550a02e322a898
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-14 14:50:51 +0300

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,20 @@
 import { AppNav, AuthNav, UserMenu } from 'components';
 import { useAuth } from 'hooks';
 
+import AppBar from '@mui/material/AppBar';
+import Container from '@mui/material/Container';
+import Toolbar from '@mui/material/Toolbar';
+
 export function Header() {
   const { isLoggedIn } = useAuth();
   return (
-    <div>
-      <AppNav />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
-    </div>
+    <AppBar position="fixed">
+      <Container maxWidth="sm">
+        <Toolbar disableGutters>
+          <AppNav />
+          {isLoggedIn ? <UserMenu /> : <AuthNav />}
+        </Toolbar>
+      </Container>
+    </AppBar>
   );
 }
