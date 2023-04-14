f4c46fbdf6b08f91d5c3981cd0a414b59c3cf36e
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-14 14:50:43 +0300

    Update Register.jsx

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,24 @@
 import { RegisterForm } from 'components';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
 
 export default function Register() {
   return (
-    <div>
-      <h2>Registration</h2>
+    <Box
+      sx={{
+        display: { xs: 'flex' },
+        alignItems: 'center',
+        flexDirection: 'column',
+      }}
+    >
+      <Divider sx={{ width: 1, pt: 2, pb: 1 }}>
+        <Typography variant="h5" sx={{ fontWeight: 700 }}>
+          Registration
+        </Typography>
+      </Divider>
       <RegisterForm />
-    </div>
+      <Divider sx={{ width: 1, pt: 2, pb: 1 }} />
+    </Box>
   );
 }
