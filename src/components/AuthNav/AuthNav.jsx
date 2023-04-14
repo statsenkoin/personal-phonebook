68e046aa7baa9ab647bf06af55d9b04d1153ff8f
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-14 14:51:06 +0300

    Update AuthNav.jsx

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -1,10 +1,21 @@
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import { LinkStyled } from '../AppNav/AppNav.styled';
 
 export const AuthNav = () => {
   return (
-    <div>
-      <Link to="/register">Register</Link>
-      <Link to="/login">Log In</Link>
-    </div>
+    <Box component="nav" sx={{ flexWrap: 'wrap', display: { xs: 'flex' } }}>
+      <LinkStyled to="/register">
+        <Typography variant="h6" sx={{ mr: 1 }}>
+          Register
+        </Typography>
+      </LinkStyled>
+      <LinkStyled to="/login">
+        <Typography variant="h6" sx={{ mr: 1 }}>
+          LogIn
+        </Typography>
+      </LinkStyled>
+    </Box>
   );
 };
