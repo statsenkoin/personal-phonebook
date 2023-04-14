b2fd721c0127bf60aa6ed2fa75c78cba316d2517
Ihor Statsenko <statsenkoin@gmail.com>
2023-04-14 14:51:15 +0300

    Update AppNav.jsx

--- src/components/AppNav/AppNav.jsx
+++ src/components/AppNav/AppNav.jsx
@@ -1,18 +1,29 @@
-import { Link } from 'react-router-dom';
 import { useAuth } from 'hooks';
+
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import AutoStoriesIcon from '@mui/icons-material/AutoStories';
+import { LinkStyled } from './AppNav.styled';
 
 export function AppNav() {
   const { isLoggedIn } = useAuth();
 
   return (
-    <nav>
-      {/* Link to Home */}
-      <Link to="/">
-        <span>
-          <span role="img" aria-label="icon"></span>My Phonebook
-        </span>
-      </Link>
-      {isLoggedIn && <Link to="/contacts">Contacts</Link>}
-    </nav>
+    <Box
+      component="nav"
+      sx={{ flexGrow: 1, flexWrap: 'wrap', display: { xs: 'flex' } }}
+    >
+      <LinkStyled to="/">
+        <AutoStoriesIcon sx={{ display: { md: 'flex' }, mr: 1 }} />
+        <Typography variant="h6" sx={{ mr: 1 }}>
+          Phonebook
+        </Typography>
+      </LinkStyled>
+      {isLoggedIn && (
+        <LinkStyled to="/contacts">
+          <Typography variant="h6">Contacts</Typography>
+        </LinkStyled>
+      )}
+    </Box>
   );
 }
